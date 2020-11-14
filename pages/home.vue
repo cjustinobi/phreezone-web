a61b9839a8c10224d3d4381fe04ab7c347680f56
cjustinobi <cjustinobi@gmail.com>
2020-11-14 20:41:32 +0100

    Add home page

---
+++ pages/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul id="demo">
+    <tree-item
+      class="item"
+      :item="treeData"
+    ></tree-item>
+  </ul>
+</template>
+
+<script>
+
+import TreeItem from '@/components/shared/TreeMenu'
+export default {
+  name: 'home',
+  layout: 'dashboard',
+  components: {
+    TreeItem
+  },
+  data() {
+    return {
+      treeData: ''
+    }
+  },
+  methods: {
+    async getTrees() {
+      this.treeData = (await this.$axios.$get('tree/getTrees'))[0]
+    },
+  },
+  mounted() {
+    this.getTrees()
+  }
+}
+</script>
+
+<style >
+
+</style>
