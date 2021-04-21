3ca652843a483b83f1e3ae59a7ac006bd8538573
cjustinobi <cjustinobi@gmail.com>
2021-04-21 11:11:13 +0100

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,12 +77,12 @@ export default {
   axios: {
     baseURL: process.env.NODE_ENV === 'dev'
       ? 'http://localhost:8000/api/'
-      : 'https://phreezone.herokuapp.com/api/'
+      : 'http://phreezone.africinnovate.xyz/api/'
   },
   publicRuntimeConfig: {
     imagePath: process.env.NODE_ENV === 'dev'
       ? 'http://localhost:8000'
-      : 'https://phreezone.herokuapp.com',
+      : 'http://phreezone.africinnovate.xyz',
   },
   router: {
     middleware: ['auth']
