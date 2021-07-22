27f409c3f900d64397dcfbc309c829752fcdd403
cjustinobi <cjustinobi@gmail.com>
2021-07-22 11:58:34 +0100

    Add staging branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,12 +78,12 @@ export default {
   axios: {
     baseURL: process.env.NODE_ENV === 'dev'
       ? 'http://localhost:8000/api/'
-      : 'https://phreezone_staging.africinnovate.xyz/api/'
+      : 'https://phreezone-staging.africinnovate.xyz/api/'
   },
   publicRuntimeConfig: {
     imagePath: process.env.NODE_ENV === 'dev'
       ? 'http://localhost:8000'
-      : 'https://phreezone_staging.africinnovate.xyz',
+      : 'https://phreezone-staging.africinnovate.xyz',
   },
   router: {
     middleware: ['auth']
