e664f4324d440090f7eb61808ee8a6a4e9c7f7af
cjustinobi <cjustinobi@gmail.com>
2021-12-18 19:52:44 +0100

    Dynamics settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -2,6 +2,17 @@
   <div>
     <a-page-header sub-title="Settings"/>
     <ChangePassword />
+
+    <p>Dollar Rate: <b>{{ dollar }}</b></p>
+    <div class="mb">
+      <a-input v-model="dollarRate" type="number" style="width: 200px" placeholder="Update dollar rate"/>
+      <a-button @click="updateDollarRate" type="primary">Update</a-button>
+    </div>
+    <p>Percentage Rate: <b>{{ percentage }}</b></p>
+    <div class="mb">
+      <a-input v-model="percentageRate" type="number" style="width: 200px" placeholder="Percentage rate"/>
+      <a-button @click="updatePercentageRate" type="primary">Update</a-button>
+    </div>
   </div>
 </template>
 
@@ -12,10 +23,50 @@
     layout: 'dashboard',
     components: {
       ChangePassword
+    },
+    data() {
+      return {
+        dollar: '',
+        dollarRate: '',
+        percentage: '',
+        percentageRate: '',
+      }
+    },
+    methods: {
+      async updateDollarRate() {
+        if (!this.dollarRate) return this.$message.error('Enter valid input')
+        const res = this.$axios.$put('admin/dollarRate', {dollarRate: parseInt(this.dollarRate)})
+        if (res) {
+          this.getDollarRate()
+          this.dollarRate = ''
+          this.$message.success('Saved successfully')
+        }
+      },
+      async updatePercentageRate() {
+        if (!this.percentageRate) return this.$message.error('Enter valid input')
+        const res = this.$axios.$put('admin/percentageRate', {percentageRate: this.percentageRate})
+        if (res) {
+          this.getPercentageRate()
+          this.percentageRate = ''
+          this.$message.success('Saved successfully')
+        }
+      },
+      async getDollarRate() {
+        this.dollar = (await this.$axios.$get('admin/dollarRate')).data
+      },
+      async getPercentageRate() {
+        this.percentage = (await this.$axios.$get('admin/percentageRate')).data
+      },
+    },
+    beforeMount() {
+      this.getDollarRate()
+      this.getPercentageRate()
     }
   }
 </script>
 
 <style scoped>
-
+  .mb {
+    margin-bottom: 10px;
+  }
 </style>
--- pages/stream-upgrade-order.vue
+++ pages/stream-upgrade-order.vue
@@ -94,6 +94,7 @@
     layout: 'dashboard',
     data() {
       return {
+        percentageRate: '',
         agentWallet: '',
         upgradeMember: '',
         loading: false,
@@ -229,8 +230,8 @@
         if (item.price < 125) return
         item.user_id = this.userId
         item.code = this.setCode(item).code
-        item.pv = item.qty ? ((+item.price / 125) * 0.25).toFixed(2) * +item.qty :  ((+item.price / 125) * 0.25).toFixed(2)// Formula product PV
-        item.amount = item.qty ? (+item.price + (0.25 * item.price)) * +item.qty : +item.price + (0.25 * item.price) // 0.25 = sales price.
+        item.pv = item.qty ? ((+item.price / 125) * this.percentageRate).toFixed(2) * +item.qty :  ((+item.price / 125) * this.percentageRate).toFixed(2)// Formula product PV
+        item.amount = item.qty ? (+item.price + (this.percentageRate * item.price)) * +item.qty : +item.price + (this.percentageRate * item.price) // 0.25 = sales price.
         this.dynamicValidateForm.data[index] = item
 
         this.setSubDetail(item, index)
@@ -255,8 +256,8 @@
           if (item.price < 125) return
           item.user_id = this.userId
           item.code = this.setCode(item).code
-          item.pv = item.qty ? ((+item.price / 125) * 0.25).toFixed(2) * +item.qty :  ((+item.price / 125) * 0.25).toFixed(2)// Formula product PV
-          item.amount = item.qty ? (+item.price + (0.25 * item.price)) * +item.qty : +item.price + (0.25 * item.price) // 0.25 == sales price %
+          item.pv = item.qty ? ((+item.price / 125) * this.percentageRate).toFixed(2) * +item.qty :  ((+item.price / 125) * this.percentageRate).toFixed(2)// Formula product PV
+          item.amount = item.qty ? (+item.price + (this.percentageRate * item.price)) * +item.qty : +item.price + (this.percentageRate * item.price) // 0.25 == sales price %
           this.dynamicValidateForm.data[index] = item
 
         }
@@ -276,6 +277,9 @@
           this.$message.error('Invalid Referral code entered')
         }
       },
+      async getPercentageRate() {
+        this.percentageRate = (await this.$axios.$get('admin/percentageRate')).data
+      }
     },
     computed: {
       agentBalance() {
@@ -286,7 +290,9 @@
       }
     },
     beforeMount() {
+      this.getPercentageRate()
       this.getAgentWallet()
+
       if(!this.isStockist) {
         this.$router.push('/home')
       }
