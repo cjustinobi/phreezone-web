b6f667675d0965f6a5ffe6179615a6c0a0ffc9dd
cjustinobi <cjustinobi@gmail.com>
2022-02-18 01:44:10 +0100

    user update

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -21,10 +21,17 @@
         <a-input v-model="user.lga" placeholder="LGA" />
       </a-form-model-item>
       <a-form-model-item label="Bank Name">
-        <a-input v-model="user.bank_name" placeholder="Bank Name" />
+        <select v-model="user.bank_name" class="form-control" id="bank" @change="getAccountName">
+          <option>Select Bank</option>
+          <option v-for="(bank, i) in banks" :key="i">{{ bank.name }}</option>
+        </select>
+<!--        <a-input v-model="user.bank_name" placeholder="Bank Name" />-->
       </a-form-model-item>
       <a-form-model-item label="Account Number">
-        <a-input v-model="user.account_number" placeholder="Account Number" />
+        <a-input v-model="user.account_number" @change="getAccountName" placeholder="Account Number" />
+      </a-form-model-item>
+      <a-form-model-item label="Account Name">
+        <a-input v-model="user.account_name" placeholder="Account Name" disabled />
       </a-form-model-item>
       <a-form-model-item label="Package" v-if="user && user.package">
         <a-input :value="user.package.name" disabled/>
@@ -52,7 +59,8 @@
       return {
         user: '',
         pkgs: '',
-        selectedPkgId: ''
+        selectedPkgId: '',
+        banks: ''
       }
     },
     methods: {
@@ -65,40 +73,32 @@
       async updateUser() {
         if (this.isValidFields() == true) {
 
-          let formData = new FormData();
-          formData.append('id', this.user.id)
-          formData.append('first_name', this.user.first_name)
-          formData.append('last_name', this.user.last_name)
-          formData.append('phone', this.user.phone)
-          formData.append('email', this.user.email)
-          formData.append('_method', 'PUT')
-
-          let res = await this.$axios.$post(`user/${this.$route.params.id}`, formData)
+          let res = await this.$axios.$put(`user/${this.$route.params.id}`, this.user)
           res.success ? this.$message.success(res.message) : this.$message.error(res.message)
-          this.$router.push('/users')
+          this.$router.push(`/users/${this.$route.params.id}`)
         }
       },
 
-      async getAccountName() {
-        if (
+      getAccountName() {
+        if (this.banks.length) {
+          setTimeout(async function () {
+            const bank = this.banks.find(bank => bank.name == this.user.bank_name)
 
-          this.banks.length
-        ) {
-          const bank = this.banks.find(bank => bank.name == this.details.bank_name)
-
-          if (bank) {
-            const bank_code = bank.code
-            const res = await this.$axios.$post('/banks', {
-              account_number: this.details.account_number,
-              bank_code
-            })
-            if (res.status) {
-              this.details.account_name = res.data.account_name
-            } else {
-              this.details.account_name = ''
+            if (bank) {
+              const bank_code = bank.code
+              const res = await this.$axios.$post('/banks', {
+                account_number: this.user.account_number,
+                bank_code
+              })
+              if (res.status) {
+                this.user.account_name = res.data.account_name
+              } else {
+                this.user.account_name = ''
+              }
             }
-          }
+          }, 5000)
         }
+
       },
       isValidFields() {
         let valid = true
@@ -115,13 +115,20 @@
           valid = false
           msgs.push('Phone is required')
         }
+        if (this.user.bank_name == '') {
+          valid = false
+          msgs.push('Bank name is required')
+        }
         if (!valid) {
           this.$store.dispatch('notification/setStatus', {
             messages: msgs, success: false
           })
         }
         return valid
-      }
+      },
+      async getBanks() {
+        this.banks = (await this.$axios.$get('/banks')).data
+      },
     },
     computed: {
       formItemLayout() {
@@ -137,6 +144,7 @@
     beforeMount() {
       this.getUser()
       this.getPackages()
+      this.getBanks()
     },
     watch: {
       user: {
