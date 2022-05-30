65719cd7d98428cdbd72dded09771faac97ab2f2
cjustinobi <cjustinobi@gmail.com>
2022-05-30 04:41:39 +0100

    download orders

---
+++ pages/stockist-order.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <a-page-header sub-title="Stockist Order"/>
+
+    <a-row>
+      <a-col span="6">
+        <a-input
+          placeholder="Enter week number"
+          v-model="week"
+          style="width: 188px; margin-bottom: 8px;"
+          @change="getItems"
+          min="1"
+          type="number"/>
+      </a-col>
+
+<!--      <a-col>-->
+        <download-excel
+          class="btn btn-primary"
+          :data="items"
+          :fields="excelFields"
+          worksheet="My Worksheet"
+          :name="`week-${week}.xls`"
+        >
+          Download
+        </download-excel>
+<!--      </a-col>-->
+    </a-row>
+
+    <a-table
+      v-if="items"
+      :columns="columns"
+      :data-source="items"
+      :rowKey="record => record.id"
+      :scroll="{ x: 1500, y: 300 }"
+      size="small">
+
+      <span slot="stockist" slot-scope="stockist">
+        {{ stockist.full_name }}<br>
+        <a-tag color="green">{{ stockist.referral }}</a-tag>
+      </span>
+      <span slot="date" slot-scope="date">{{ formatDate(date) }}</span>
+      <span slot="products" slot-scope="products">
+        <table>
+          <thead>
+          <tr>
+            <th width="34%">Product</th>
+            <th width="33%">Code</th>
+            <th width="33%">Qty</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="prod in products">
+            <td class="td">{{ prod.name }}</td>
+            <td class="td">{{ prod.code }}</td>
+            <td class="td">{{ prod.qty }}</td>
+          </tr>
+          </tbody>
+        </table>
+        {{product}}
+      </span>
+    </a-table>
+  </div>
+</template>
+
+<script>
+  const columns = [
+    {title: 'Date', dataIndex: 'created_at', scopedSlots: { customRender: 'date'}},
+    {title: 'Sold By', dataIndex: 'sold_by', scopedSlots: { customRender: 'stockist'}},
+    {title: 'Product Name', dataIndex: 'items', scopedSlots: { customRender: 'products'}},
+    // {title: 'Qty', dataIndex: 'items.qty'}
+  ]
+
+  import DateFormat from '@/mixins/dateFormat'
+  export default {
+    name: 'stockist-orders',
+    layout: 'dashboard',
+    mixins: [DateFormat],
+    data() {
+      return {
+        items: '',
+        columns,
+        week: '',
+        dateFormat: 'p d MMM, Y',
+        excelFields: {
+          Date: 'created_at',
+          Fullname: 'sold_by.full_name',
+          ID: 'sold_by.referral',
+          Product: {
+            field: 'items',
+            callback: val => {
+              return JSON.stringify(val.map(item => item.name))
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      getItems() {
+        const self = this
+        setTimeout(async function () {
+          self.items = (await self.$axios.$post(`admin/stockistOrders`, {
+            weekNumber: self.week
+          })).data
+        }, 3000)
+      },
+      async setWeek() {
+        this.week = await this.$axios.$get('date')
+      },
+    },
+    mounted() {
+      this.setWeek()
+      this.getItems()
+    }
+  }
+</script>
+
+<style scoped>
+  .td {
+    border-bottom: 1px solid #dfe0e2;
+  }
+</style>
